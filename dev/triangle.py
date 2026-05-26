31b5297cda282c281b0ce7a8857b20d42cdcf56c
freedaprajesh <prajeshkollath@gmail.com>
2026-05-26 12:27:53 +0000

    feat: Create inverted triangle Python script

---
+++ dev/triangle.py
@@ -0,0 +1,5 @@
+def inverted_triangle(rows=5):
+    for i in range(rows, 0, -1):
+        print('* ' * i)
+
+inverted_triangle()
---
+++ inverted_triangle.py
@@ -0,0 +1,8 @@
+# Inverted Triangle Pattern
+
+def inverted_triangle(n):
+    for i in range(n, 0, -1):
+        print('*' * i)
+
+# Example usage
+inverted_triangle(5)
